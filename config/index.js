c43c82bbcb422b84d3c5112a736730e59201bad4
juanAlcala0204 <juan.alcala0204@gmail.com>
2020-11-03 19:15:18 -0500

    Primer Commit inicial del proyecto
    Se configura las bases de la API bajando dependencias como express, eslint , prettier, nodemon ( para desarrollo ) entre otras.

---
+++ config/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+
+// Se crea objeto de configuración.
+const config = {
+    // Variable que comprueba si está en producción o en desarrollo
+    dev: process.env.NODE_ENV !== 'production',
+    // Variable de entorno port.
+    port: process.env.PORT || 3000
+};
+
+module.exports = { config };
